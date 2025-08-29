593e9ad0db4c5c9ac5500e54e75a2e3293133ace
Bikash <bikashkdas23@gmail.com>
2025-08-29 23:51:15 +0530

    `Added chatRouter and updated app.js to include it`

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 const paymentRouter = require("./routes/payment");
+const chatRouter = require("./routes/chat");
 const http=require('http');
 const intialiseSocket = require("./utils/socket");
 
@@ -56,6 +57,7 @@ app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 app.use('/',paymentRouter);
+app.use("/",chatRouter);
 
 const server=http.createServer(app);
 intialiseSocket(server);
---
+++ src/routes/chat.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const chatRouter = express.Router();
+const Chat = require('../model/chat');
+const { authMiddleware } = require('../middleware/authMiddleware');
+
+chatRouter.get('/chat/:targetUserId', authMiddleware, async (req, res) => {
+    const { targetUserId } = req.params;
+    const userId = req.user._id;
+
+    try {
+        let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] }, 
+        }).populate({
+            path:"messages.senderId",
+            select:"firstName lastName"
+        });
+
+        if (!chat) {
+            chat = new Chat({
+                participants: [userId, targetUserId],
+                messages: [],
+            });
+            await chat.save();
+        }
+
+        res.json(chat);
+    } catch (err) {
+        res.status(500).send('Error: ' + err.message);
+    }
+});
+
+module.exports = chatRouter;
